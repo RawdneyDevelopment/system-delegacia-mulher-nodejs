e558fa060049e5835cd23080f84f1358f19375fb
Rawdney Mendes <mendesrawdney@gmail.com>
2022-12-20 01:10:41 -0300

    add routes to receive request, responses

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,3 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 const userController = require('../controllers/userController');
+
+routes.post('/register', userController.createUser);
+
+module.exports = routes; 
