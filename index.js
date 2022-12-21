9835126e4f31e24191fa333510752f423d92c7da
vinicius gabriel <vinicius-gabriel-guilherme@hotmail.com>
2022-12-20 22:24:15 -0300

    new index configuration

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-require('./src/database/index');
-const routes = require('./src/router/routes');
+require("dotenv").config();
+require("./src/database/index");
+const userRoutes = require("./src/router/user.routes");
+const addressRoutes = require("./src/router/address.routes");
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.options("*", cors());
 
 const PORT = process.env.PORT || 3030;
 
-app.use(routes);
+app.use("/v1/api", userRoutes);
+app.use("/v1/api", addressRoutes);
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em: ${PORT}`)
+  console.log(`Servidor rodando em: ${PORT}`);
 });
