c77e3689df3e605f9dc058d6654b77fdf05508d1
vinicius gabriel <vinicius-gabriel-guilherme@hotmail.com>
2022-12-20 22:25:57 -0300

    create new routes user and address

--- src/router/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-const userController = require('../controllers/userController');
-
-routes.post('/register', userController.createUser);
-
-module.exports = routes; 
