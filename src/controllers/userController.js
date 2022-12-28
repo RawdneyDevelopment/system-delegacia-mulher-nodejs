eb29d1e497f801eaa5d69db456e61919873a8bc7
Rawdney <mendesrawdney@gmail.com>
2022-12-27 21:23:05 -0300

    added: organization methods controllers, new modelation to iteractable user.

---
+++ src/controllers/userController.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const User = require("../models/user");
+
+
+
+exports.createUser = async (req, res) => {
+};
+
+exports.readUser = async (req, res) => {
+
+};
+
+exports.updateUser = async (req, res) => {
+};
+
+
+exports.listUsers = async (req, res) => {
+};
+
+exports.findUsersById = async (req, res) => {
+
+};
